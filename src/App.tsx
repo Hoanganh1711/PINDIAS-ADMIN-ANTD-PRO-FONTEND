747b49c2050361ee8144e20cc0198d920c34db3e
NguyenCongJP <nguyenquecong.nqc@gmail.com>
2022-07-12 11:23:37 +0700

    update project with lastest version

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,44 @@
-import { Button } from 'antd'
-import Router from './router'
+import { Layout } from 'antd'
+import { Suspense } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import routes from './config/routes'
 import { Link } from '@/components'
+import '@ant-design/pro-components/dist/components.css'
+
+const { Content } = Layout
 
 function App() {
-  console.log('import.meta.env', import.meta.env.VITE_APP_NAME)
-  return (
-    <div className="App">
-      <Link to="/">Home Page</Link>
-      <Link to="/about">About Page</Link>
-      <Button type="primary">按钮</Button>
-      <Router />
-    </div>
-  )
+    console.log('import.meta.env', import.meta.env.VITE_APP_NAME)
+    const myRoutes = routes.map((item: any) => {
+        console.log(item)
+
+        return <Route key={item.path} {...item} element={<item.component />} />
+    })
+    return (
+        <>
+            <Suspense fallback={<div>Loading...</div>}>
+                <BrowserRouter>
+                    <Layout
+                        style={{
+                            minHeight: '100vh',
+                        }}
+                    >
+                        <Layout className="site-layout">
+                            <Content
+                                style={{
+                                    margin: '40px',
+                                }}
+                            >
+                                <Link to="/">Home Page</Link>
+                                <Link to="/about">About Page</Link>
+                                <Routes>{myRoutes}</Routes>
+                            </Content>
+                        </Layout>
+                    </Layout>
+                </BrowserRouter>
+            </Suspense>
+        </>
+    )
 }
 
 export default App
--- src/config/routes.ts
+++ src/config/routes.ts
@@ -1,33 +1,29 @@
 import { lazy } from 'react'
 
-const Home = lazy(() => import('../pages/home'))
-const About = lazy(() => import('../pages/about'))
-const NotFound = lazy(() => import('../pages/not_found'))
+const Home = lazy(() => import('../pages/Home'))
+const About = lazy(() => import('../pages/About'))
+const NotFound = lazy(() => import('../pages/NotFound'))
 
 const routes = [
-  {
-    path:'/',
-    exact: true,
-    component: Home,
-    name:'Home'
-  },
-  {
-    path:'/home',
-    component: Home,
-    name:'Home'
-  },
-  {
-    path:'/about',
-    component: About,
-    name:'About'
-  },
-  {
-    path: '/404',
-    component: NotFound,
-  },
-  {
-    path: '*',
-    component: NotFound,
-  }
+    {
+        path: '/',
+        exact: true,
+        component: Home,
+        name: 'Home',
+    },
+    {
+        path: '/home',
+        component: Home,
+        name: 'Home',
+    },
+    {
+        path: '/about',
+        component: About,
+        name: 'About',
+    },
+    {
+        path: '/404',
+        component: NotFound,
+    },
 ]
 export default routes
